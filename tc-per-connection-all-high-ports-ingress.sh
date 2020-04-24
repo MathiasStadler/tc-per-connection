102fbebca1d9fdaf3c9ff1ecdff65cbbc9e9f76c
Mathias Stadler <email@mathias-stadler.de>
2020-04-24 12:18:07 +0000

    add ingress

--- tc-per-connection-all-high-ports-ingress.sh
+++ tc-per-connection-all-high-ports-ingress.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# settings
 dev=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 ip_port=49152:65535
 rate_limit=512kbit
@@ -7,21 +8,47 @@ rate_ceil=1024kbit
 htb_class=10
 # max_byte=10485760
 max_byte=50000
+# Interface virtual for incomming traffic
+tin1="ifb0"
+
+# load modul for ifb
+modprobe ifb numifbs=1
+
+# set interface up
+ip link set dev $tin1 up
+
+# for save Clean interface tc rule
+tc qdisc del root dev $int1
+tc qdisc del dev $int1 ingress
+tc qdisc del dev $int1 root
+tc qdisc del dev $int1 root
+
 
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
 
-echo "Use interfaces $dev"
+echo "Use egress interfaces $dev"
+echo "Use ingress interfaces $int1"
 
 if [ "$1" = "enable" ]; then
     echo "enabling rate limits"
+    # delete egress
     tc qdisc del dev $dev root > /dev/null 2>&1
     tc qdisc add dev $dev root handle 1: htb
+    # delete ingress
+    tc qdisc del dev $int1 root
+    tc qdisc del dev $int1 root handle 1: htb
 
+    # add tc egress
     tc class add dev $dev parent 1: classid 1:$htb_class htb rate $rate_limit ceil $rate_ceil
     tc filter add dev $dev parent 1: prio 0 protocol ip handle $htb_class fw flowid 1:$htb_class
+    # add tc ingress
+    tc class add dev $int1 parent 1: classid 1:$htb_class htb rate $rate_limit ceil $rate_ceil
+    tc filter add dev $int1 parent 1: prio 0 protocol ip handle $htb_class fw flowid 1:$htb_class
+    
+
 
     #iptables -t mangle -A OUTPUT -p tcp --sport $ip_port -j MARK --set-mark $htb_class
 
