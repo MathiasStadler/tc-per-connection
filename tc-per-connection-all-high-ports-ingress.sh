ddb31c1b9735ba4a95af958b6c321eea89966793
Mathias Stadler <email.mathias-stadler.de>
2020-04-27 08:32:12 +0000

    save

--- tc-per-connection-all-high-ports-ingress.sh
+++ tc-per-connection-all-high-ports-ingress.sh
@@ -7,8 +7,8 @@ dev=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 ip_port=1025:65535
 # rate_limit=1024kbit
 rate_egress_limit=3072kbit
-rate_ingress_limit=1024kbit
-
+# rate_ingress_limit=1024kbit
+rate_ingress_limit=512kbit
 # @ TODO not used rate_ceil=1024kbit
 # @TODO old check to delete  htb_class=10
 # max_byte=10485760
